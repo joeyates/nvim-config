36c21999b74b49b0711227206734948fa1484288
Joe Yates <joe.g.yates@gmail.com>
2023-09-01 22:57:35 +0200

    Use telescope

--- lua/navigation.lua
+++ lua/navigation.lua
@@ -1,15 +1,31 @@
 navigation = {}
 
 function navigation.register(use)
-  use 'ctrlpvim/ctrlp.vim'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end
 
 function navigation.configure()
-  vim.cmd([[
-    let g:ctrlp_custom_ignore = 'deps'
-  ]])
-  -- open ctrlp
-  vim.keymap.set('n', '<Leader>f', ':CtrlP<CR>')
+  -- https://github.com/nvim-telescope/telescope.nvim
+  local telescope = require("telescope")
+  local builtin = require('telescope.builtin')
+  telescope.setup({
+    pickers = {
+      find_files = {
+        find_command = {
+          "find", ".",
+          "-name", "_build", "-prune", "-o",
+          "-name", "deps", "-prune", "-o",
+          "-name", ".git", "-prune", "-o",
+          "-type", "f", "-print"
+        }
+      }
+    }
+  })
+  -- search for files
+  vim.keymap.set('n', '<leader>f', builtin.find_files, {})
   -- cycle through buffers
   vim.keymap.set('n', '<Leader>p', ':bprevious<CR>')
   vim.keymap.set('n', '<Leader><Left>', ':bprevious<CR>')
