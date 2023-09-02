d3b710c228636132354072aaaa837b14c867f64a
Joe Yates <joe.g.yates@gmail.com>
2023-09-02 11:44:30 +0200

    Add note about hiding border

--- lua/display.lua
+++ lua/display.lua
@@ -10,6 +10,19 @@ function display.configure()
   -- Use truecolor
   vim.opt.termguicolors = true
 
+  --[[
+  nvim only paints an area that is an exact number of
+  characters wide and high. Any space left over
+  continues to have the background colour of the terminal.
+  If nvim's colour scheme sets a different background colour,
+  this results in an unsightly border.
+  So, the terminal background coulour must be set to match
+  the nvim theme.
+  For onedark, check the value of `bg0` for
+  the chosen style here:
+  https://github.com/navarasu/onedark.nvim/blob/master/lua/onedark/palette.lua
+  ]]
+
   -- https://github.com/navarasu/onedark.nvim
   local onedark = require('onedark')
   onedark.setup {
