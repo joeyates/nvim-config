efc0a95d294966cf811441c10542b0a312637a42
Joe Yates <joe.g.yates@gmail.com>
2023-09-07 20:00:51 +0200

    Set options via Lua

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -6,12 +6,19 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 -- indent a new line the same amount as the line just typed
 vim.opt.autoindent = true
--- do not automatically wrap text when typing
-vim.opt.formatoptions:remove("t")
-vim.cmd([[
-" set an 80 column border for good coding style
-set cc=80
-" do not automatically wrap on load
-set nowrap
-]])
+-- set an 80 column border
+vim.opt.colorcolumn = "80"
+
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    callback = function(ev)
+      -- :help fo-table
+      -- do not automatically wrap text when typing
+      vim.opt.formatoptions:remove("t")
+      -- do not automatically wrap long lines
+      vim.opt_local.wrap = false
+    end
+  }
+)
 
