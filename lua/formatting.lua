759111b7f4e80ac1b640d2f47189468cda4aa04a
Joe Yates <joe.g.yates@gmail.com>
2023-09-07 20:01:06 +0200

    Don't add comment characters automatically

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -16,6 +16,10 @@ vim.api.nvim_create_autocmd(
       -- :help fo-table
       -- do not automatically wrap text when typing
       vim.opt.formatoptions:remove("t")
+      -- do not insert current comment leader after <Enter>
+      vim.opt.formatoptions:remove("r")
+      -- do not insert current comment leader after 'o' or 'O'
+      vim.opt.formatoptions:remove("o")
       -- do not automatically wrap long lines
       vim.opt_local.wrap = false
     end
