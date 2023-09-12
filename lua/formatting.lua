cb010cad9e566cd276603e830ae5a96e38e3cf56
Joe Yates <joe.g.yates@gmail.com>
2023-09-12 13:13:45 +0200

    Set formatting when creating new buffers

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -9,20 +9,19 @@ vim.opt.autoindent = true
 -- set an 80 column border
 vim.opt.colorcolumn = '80'
 
-vim.api.nvim_create_autocmd(
-  'BufReadPost',
-  {
-    callback = function(ev)
-      -- :help fo-table
-      -- do not automatically wrap text when typing
-      vim.opt.formatoptions:remove('t')
-      -- do not insert current comment leader after <Enter>
-      vim.opt.formatoptions:remove('r')
-      -- do not insert current comment leader after 'o' or 'O'
-      vim.opt.formatoptions:remove('o')
-      -- do not automatically wrap long lines
-      vim.opt_local.wrap = false
-    end
-  }
-)
+local set_buffer_formatting_options = function(ev)
+  -- :help fo-table
+  -- do not automatically wrap text when typing
+  vim.opt.formatoptions:remove('t')
+  -- do not insert current comment leader after <Enter>
+  vim.opt.formatoptions:remove('r')
+  -- do not insert current comment leader after 'o' or 'O'
+  vim.opt.formatoptions:remove('o')
+  -- do not automatically wrap long lines
+  vim.opt_local.wrap = false
+end
 
+vim.api.nvim_create_autocmd(
+  'BufReadPost,BufNewFile',
+  {callback = set_buffer_formatting_options}
+)
